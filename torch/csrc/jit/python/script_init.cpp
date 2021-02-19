b388fbb596a29d0e9712c0eb63dcf37cd3711ae4
Tugsbayasgalan Manlaibaatar <tmanlaibaatar@fb.com>
2021-02-18 18:05:43 -0800

    Add property binding in torchbind
    Summary:
    
    Test Plan:
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:
    
    ghstack-source-id: 8ecfb5528c643f929d12eaed323630456a45ff09
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/50670

--- torch/csrc/jit/python/script_init.cpp
+++ torch/csrc/jit/python/script_init.cpp
@@ -17,6 +17,7 @@
 #include <torch/csrc/jit/ir/constants.h>
 #include <torch/csrc/jit/ir/irparser.h>
 #include <torch/csrc/jit/passes/inliner.h>
+#include <torch/csrc/jit/python/module_python.h>
 #include <torch/csrc/jit/python/pybind_utils.h>
 #include <torch/csrc/jit/python/python_tracer.h>
 #include <torch/csrc/jit/runtime/graph_executor.h>
@@ -32,6 +33,7 @@
 #include <ATen/core/function_schema.h>
 #include <ATen/core/qualified_name.h>
 
+#include <Exceptions.h>
 #include <pybind11/functional.h>
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
@@ -40,6 +42,7 @@
 #include <cstddef>
 #include <memory>
 #include <sstream>
+#include <stdexcept>
 #include <string>
 #include <tuple>
 #include <utility>
@@ -783,9 +786,53 @@ void initJitScriptBindings(PyObject* module) {
                   if (auto method = self.find_method(name)) {
                     return py::cast(*method);
                   }
+                  if (self.has_property(name)) {
+                    auto prop = self.get_property(name);
+                    auto getter_func = py::cast(std::get<1>(prop));
+                    auto setter_func = py::cast(std::get<2>(prop));
+                    py::object property_func =
+                        py::module::import("builtins").attr("property");
+                    py::object property =
+                        property_func(getter_func, setter_func);
+                    return property.attr("fget")();
+                  }
                   return toPyObject(self.attr(name));
                 } catch (const ObjectAttributeError& err) {
                   throw AttributeError("%s", err.what());
+                }
+              })
+          .def(
+              "__setattr__",
+              [](Object& self, const std::string& name, py::object value) {
+                try {
+                  if (self.has_property(name)) {
+                    auto prop = self.get_property(name);
+                    auto getter_func = py::cast(std::get<1>(prop));
+                    auto setter_func = py::cast(std::get<2>(prop));
+                    py::object property_func =
+                        py::module::import("builtins").attr("property");
+                    py::object property =
+                        property_func(getter_func, setter_func);
+                    property.attr("fset")(value);
+                    return;
+                  }
+
+                  if (self.type()->hasConstant(name)) {
+                    TORCH_CHECK(
+                        false,
+                        "Can't set constant '",
+                        name,
+                        "' which has value:",
+                        self.type()->getConstant(name));
+                  }
+                  TypePtr type = self.type()->getAttribute(name);
+                  auto ivalue = toIValue(std::move(value), type);
+                  self.setattr(name, ivalue);
+                } catch (const ObjectAttributeError& err) {
+                  throw AttributeError("%s", err.what());
+                } catch (const std::runtime_error& err) {
+                  throw AttributeError(
+                      "property %s doesn't have a setter method", name.c_str());
                 }
               })
           .def(
